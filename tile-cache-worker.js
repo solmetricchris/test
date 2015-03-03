b922978608509fa046d9e20664235b01730fe9b4
solmetricchris <christopher.schaefer@vivintsolar.com>
2015-03-02 16:09:19 -0800

    a bunch

--- tile-cache-worker.js
+++ tile-cache-worker.js
@@ -5,39 +5,35 @@ importScripts('serviceworker-cache-polyfill.js');
 console.log('SW startup');
 
 self.addEventListener('install', function (event) {
-  console.log('SW installing...');
+  console.log('tile cache installing...');
 
-  event.waitUntil(caches.open('static-v1').then(function (cache) {
-    console.log('SW installed!');
-
-    return cache.addAll([
-      '/index.html',
-      '/supertest.js'
-    ]);
+  event.waitUntil(caches.open('map-tile-cache').then(function (cache) {
+    console.log('tile cache installed!');
   }));
 });
 
 var expectedCaches = [
-  'static-v1'
+  'map-tile-cache'
 ];
 
 self.addEventListener('activate', function (event) {
-  console.log('SW activating...');
+  console.log('tile cache activating...');
   event.waitUntil(caches.keys().then(function (cacheNames) {
-    console.log('SW activated!');
-
+    console.log('tile cache activated!');
   }));
 });
 
 self.addEventListener('fetch', function (event) {
-  console.log('SW fetching...');
+  console.log('tile cache fetching...');
   var requestURL = new URL(event.request.url);
 
   // Poke a hole and allow ping and login routes to go over the network
-  if (requestURL.pathname.indexOf('ping') === -1 && requestURL.pathname.indexOf('login') === -1) {
+
     event.respondWith(caches.match(event.request).then(function (response) {
-      console.log('SW fetched ', response);
+      console.log('tile fetched from cache ', response);
       return response || new Response('Nothing in the cache for this request');
-    }));
-  }
+    }, function(response) {
+            console.log('unable to fetch tile from cache ', response);
+        })
+    );
 });
